914c6bf59c7f1352eb970fd9fd57b28789a27ddc
Elyass Scala <sen0.guide@gmail.com>
2022-12-15 16:52:02 +0100

    simple modif

--- autre/lib/UserInterface/UserInterfaceMobileFolder/user_interface_mobile.dart
+++ autre/lib/UserInterface/UserInterfaceMobileFolder/user_interface_mobile.dart
@@ -25,10 +25,10 @@ class _UserInterfaceMobileState extends State<UserInterfaceMobile> {
           animationDuration: Duration(milliseconds: 350),
           height: 60,
           items: [
-            Icon(Icons.message, size: 30),
-            Icon(Icons.square, size: 30),
-            Icon(Icons.person, size: 30),
-            Icon(Icons.settings, size: 30),
+            Icon(Icons.message_outlined, size: 30),
+            Icon(Icons.square_outlined, size: 30),
+            Icon(Icons.person_outline, size: 30),
+            Icon(Icons.settings_outlined, size: 30),
           ]
         ),
         
@@ -37,8 +37,8 @@ class _UserInterfaceMobileState extends State<UserInterfaceMobile> {
           width: 1950,
           child: 
         Column(
-          
           crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
           
           children: [
             Container(
@@ -52,43 +52,56 @@ class _UserInterfaceMobileState extends State<UserInterfaceMobile> {
                   mainAxisSpacing: 10,
                 
                   children: [
-                    Container(child: Text("Global Chat"),color: Colors.blue),
+                    Container(child: Text("Global Chat"),color: Color.fromARGB(197, 93, 192, 238)),
                     Container(child: Text("Alert Dragons"),color: Colors.red),
                     Container(child: Text("Shadow Tower"),color: Colors.purpleAccent),
                     Container(child: Text("Pinata Party"),color: Colors.yellow,),
-                    Container(child: Text("Dongeons"),color: Colors.grey),
+                    Container(child: Text("Dongeons"),color: Colors.blueGrey),
                     Container(child: Text("Delves"),color: Colors.orange),
                   ],
                 )
             ),
 
-            SizedBox(height: 30,),
+            SizedBox(height: 5,),
 
             // Bonus Days
-           Container(
+          Container(
             width: 850,
-                constraints: BoxConstraints(maxHeight: 150),
-                decoration: BoxDecoration(border: Border.all(color: Colors.white, width: 2)),
-                child:
-                  SingleChildScrollView(
-                    
-                    scrollDirection: Axis.horizontal,
-                    child:  Row(
-                        children: <Widget>[
-                          Image.asset('assets/images/bonusDays/xp.png',),
-                          Image.asset('assets/images/bonusDays/booty.png'),
-                          Image.asset('assets/images/bonusDays/dragons.png'),
-                          Image.asset('assets/images/bonusDays/fouilles.png'),
-                          Image.asset('assets/images/bonusDays/gems.png'),
-                          Image.asset('assets/images/bonusDays/adventure.png')   
-                        ],
-                    ),
-                  )           
-              )
+            constraints: BoxConstraints(maxHeight: 150),
+            decoration:
+            BoxDecoration(
+              color: Color.fromARGB(241, 46, 46, 46),
+              border: Border.all(
+                color: Color.fromARGB(241, 46, 46, 46), // 255, 46, 46, 46 
+                width: 3
+              ),
+            ),                          
+            child:
+              SingleChildScrollView(               
+                scrollDirection: Axis.horizontal,
+                child:  Row(
+
+                  children: <Widget>[
+                    Image.asset('assets/images/bonusDays/xp.png',),
+                    SizedBox(width: 2,),
+                    Image.asset('assets/images/bonusDays/booty.png'),
+                    SizedBox(width: 2,),
+                    Image.asset('assets/images/bonusDays/dragons.png'),
+                    SizedBox(width: 2,),
+                    Image.asset('assets/images/bonusDays/fouilles.png'),
+                    SizedBox(width: 2,),
+                    Image.asset('assets/images/bonusDays/gems.png'),
+                    SizedBox(width: 2,),
+                    Image.asset('assets/images/bonusDays/adventure.png')   
+                  ],
+                ),
+
+              )           
+          ),
+                SizedBox(),
           ],
         ),),
       ),
     );
-
   }
 }
